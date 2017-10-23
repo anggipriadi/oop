16cea2c7e85f4999a11573b5b867c76bacc9916d
anggi priadi <32733902+anggipriadi@users.noreply.github.com>
2017-10-23 21:40:20 +0700

    Add files via upload

---
+++ tugasoop1.java
@@ -0,0 +1,15 @@
+public class tugasoop1 {
+
+    public static void main (String[] args) {     
+    System.out.println("~~~LANGKAH-LANGKAH MEMASAK NASI~~~");
+    
+    System.out.println("1. Untuk menghasilkan nasi yang bagus pilihlah beras yang mempunyai kualitas yang bagus");
+    System.out.println("2. Cuci beras dengan merendamkannya pada air, setelah itu aduk- aduk agar berasnya bersih dari kotoran");
+    System.out.println("3. Buang air rendamannya kemudian ulangi 3-4 kali");
+    System.out.println("4. Cuci lalu masukan beras yang sudah di cuci tadi, dan berilah air sampai mencapai setengah ruas jari telunjuk tangan");
+    System.out.println("5. Masak di atas kompor dengan menggunakan api sedang");            
+    System.out.println("6. Tunggu sampai mendidih kemudian di aduk- aduk agar ridak terjadi pengerakan di lapisan bawah panci");
+    System.out.println("7. Aduk-aduk terus sampai nasi sudah benar- benar lembut dan enak");        
+    System.out.println("8. Nasi siap di hidangkan");
+  }
+}
